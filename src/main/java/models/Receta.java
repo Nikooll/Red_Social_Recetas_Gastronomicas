ef7174b0517ca9d632489a178a2cc6760e1ca683
Sebastian <joseph.narvaez@unl.edu.ec>
2024-11-03 17:46:32 -0500

    Crear receta

---
+++ src/main/java/models/Receta.java
@@ -0,0 +1,55 @@
+package models;
+
+import java.util.Date;
+
+public class Receta {
+    private Integer idReceta;
+    private String nombre;
+    private Date fechaPublicacion;
+    private String preparacion;
+    private Integer porciones;
+    private Boolean favoritos;
+
+    public Receta() {
+    }
+
+
+    public Integer getIdReceta() {
+        return idReceta;
+    }
+    public void setIdReceta(Integer idReceta) {
+        this.idReceta = idReceta;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Date getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+    public void setFechaPublicacion(Date fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+    public String getPreparacion() {
+        return preparacion;
+    }
+    public void setPreparacion(String preparacion) {
+        this.preparacion = preparacion;
+    }
+    public Integer getPorciones() {
+        return porciones;
+    }
+    public void setPorciones(Integer porciones) {
+        this.porciones = porciones;
+    }
+    public Boolean getFavoritos() {
+        return favoritos;
+    }
+    public void setFavoritos(Boolean favoritos) {
+        this.favoritos = favoritos;
+    }
+
+    
+}
